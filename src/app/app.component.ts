2d38dc89ec34cc6f120ddae5a573a8e5006774be
Alex Tetervak <tetervak@sheridancollege.ca>
2024-01-23 23:52:51 -0500

    finished the logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {CardFactory} from "./card-factory";
+import {Card} from "./card";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MyCards';
+
+  private cardFactory: CardFactory = new CardFactory();
+  card: Card = this.cardFactory.randomCard
+  imgSrc: string = AppComponent.cardImageSrc(this.card.number);
+
+  private static cardImageSrc(num: number): string {
+    return `assets/images/cards/${num}.png`
+  }
+
+  next(): void {
+    this.card = this.cardFactory.randomCard
+    this.imgSrc = AppComponent.cardImageSrc(this.card.number);
+  }
 }
---
+++ src/app/card-factory.ts
@@ -0,0 +1,25 @@
+import {Card} from "./card";
+
+export class CardFactory {
+  private static suits: string[] = [
+    'Spades', 'Hearts', 'Diamonds', 'Clubs'];
+
+  private static faces: string[] = [
+    'Ace', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten',
+    'Jack', 'Queen', 'King'
+  ]
+
+  get randomCard(): Card {
+    const rand: number = Math.floor(52 * Math.random())
+    return CardFactory.makeCard(rand);
+  }
+
+  private static makeCard(index: number): Card {
+    const card: Card = new Card()
+    card.number = index + 1;
+    card.face = CardFactory.faces[index % 13];
+    card.suit = CardFactory.suits[Math.floor(index / 13)]
+    card.name = `${card.face} of ${card.suit}`
+    return card;
+  }
+}
